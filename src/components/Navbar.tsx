8fc4367f3767dbe94c22d6c9b4b65fdbdfa38cc1
archinit <architbanait1@gmail.com>
2026-03-03 18:02:49 +0530

    Navbar-updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,32 @@
 import Image from "next/image"
 import Link from "next/link"
+import { SearchBar } from "./SearchBar"
+import { Bell, Home, ShoppingCart } from "lucide-react"
 
 export const Navbar = () => {
-    return <div>
+    return <nav className="w-full flex items-center justify-between border-b border-gray-200 pb-4">
         {/*LEFT*/}
         <Link href="/" className="flex items-center">
         <Image
             src="/logo.png"
-            alt=""
+            alt="Somename"
             width={36}
             height={36}
             className="w-6 h-6 md:w-9 md:h-9"
         />
-        <p>SomeName</p>
+        <p className="hidden md:block text-md font-semibold tracking-wide">SOMENAME</p>
         </Link>
-    </div>
+        {/* RIGHT */}
+        <div className="flex items-center gap-6">
+            <SearchBar/>
+            <Link href="/">
+                <Home className="w-4 h-4 text-gray-600"/>
+            </Link>
+            <Bell className="w-4 h-4 text-gray-600"/>
+            <ShoppingCart className="w-4 h-4 text-gray-600"/>
+            <Link href="/login">
+                Sign in
+            </Link>
+        </div>
+    </nav>
 }
