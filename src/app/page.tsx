dcd92e67caa68802c7e430ada862ba741ec42220
archinit <architbanait1@gmail.com>
2026-03-05 23:55:27 +0530

    page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="bg-red-200">
-      Homepage
+    <div className="">
+      <div className="relative aspect-3/1 mb-12">
+        <Image src="/featured.png" alt="Featured Product" fill/>
+      </div>
     </div>
   );
 }
