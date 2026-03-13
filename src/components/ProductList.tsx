f12f884cc251bfb83cabc61217890c28f146eda7
archinit <architbanait1@gmail.com>
2026-03-13 17:04:49 +0530

    Added Product List

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,3 +1,21 @@
+const products = [
+    {
+        id: 1,
+        name: "Adidas CoreFit T-shirt",
+        shortDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, eligendi possimus eum delectus voluptatibus.",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, libero dolorem cumque dicta sunt eaque culpa. Dignissimos ratione inventore officiis impedit expedita explicabo? Neque consectetur libero doloribus quidem magnam suscipit.",
+        price: 39.9,
+        sizes: ["s", "m", "l", "xl", "xxl"],
+        colors: ["gray", "purple", "green"],
+        images: {
+            gray: "/products/1g.png",
+            purple: "/products/1p.png",
+            green: "/products/1gr.png"
+        },
+    },
+    
+]
+
 export const ProductList = () => {
     return <div>
         Products Lists
