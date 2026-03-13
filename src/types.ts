6ac8810ad0dbd63687cb06f55f3e845ebdc84d1c
archinit <architbanait1@gmail.com>
2026-03-13 17:39:24 +0530

    Product Types added

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+type ProductType = {
+    id: string | number;
+    name: string;
+    shortDescription: string;
+    description: string;
+    price: number;
+    sizes: string[];
+    colors: string[];
+    images: Record<string, string>
+}
+
+export type ProductsType = ProductType[];
