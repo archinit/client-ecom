a7f1f91b4307e36848b5c6e1a4407691420b9bb5
archinit <architbanait1@gmail.com>
2026-03-13 17:16:59 +0530

    Updated Product List

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -13,7 +13,100 @@ const products = [
             green: "/products/1gr.png"
         },
     },
-    
+    {
+        id: 2,
+        name: "Puma Ultra Warm Zip",
+        shortDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, eligendi possimus eum delectus voluptatibus.",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, libero dolorem cumque dicta sunt eaque culpa. Dignissimos ratione inventore officiis impedit expedita explicabo? Neque consectetur libero doloribus quidem magnam suscipit.",
+        price: 59.9,
+        sizes: ["s", "m", "l", "xl"],
+        colors: ["gray", "green"],
+        images: {
+            gray: "/products/2g.png",
+            green: "/products/2gr.png"
+        },
+    },
+    {
+        id: 3,
+        name: "Nike Air Essentials Pullover",
+        shortDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, eligendi possimus eum delectus voluptatibus.",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, libero dolorem cumque dicta sunt eaque culpa. Dignissimos ratione inventore officiis impedit expedita explicabo? Neque consectetur libero doloribus quidem magnam suscipit.",
+        price: 69.9,
+        sizes: ["s", "m", "l", "xl", "xxl"],
+        colors: ["blue", "black", "green"],
+        images: {
+            blue: "/products/3b.png",
+            black: "/products/3bl.png",
+            green: "/products/3gr.png"
+        },
+    },
+    {
+        id: 4,
+        name: "NIke Dri Flex T-Shirt",
+        shortDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, eligendi possimus eum delectus voluptatibus.",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, libero dolorem cumque dicta sunt eaque culpa. Dignissimos ratione inventore officiis impedit expedita explicabo? Neque consectetur libero doloribus quidem magnam suscipit.",
+        price: 29.9,
+        sizes: ["s", "m", "l"],
+        colors: ["white", "pink"],
+        images: {
+            pink: "/products/4p.png",
+            white: "/products/4w.png",
+        },
+    },
+    {
+        id: 5,
+        name: "Under Armour StormFleece",
+        shortDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, eligendi possimus eum delectus voluptatibus.",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, libero dolorem cumque dicta sunt eaque culpa. Dignissimos ratione inventore officiis impedit expedita explicabo? Neque consectetur libero doloribus quidem magnam suscipit.",
+        price: 49.9,
+        sizes: ["s", "m", "l"],
+        colors: ["red", "orange", "black"],
+        images: {
+            red: "/products/5r.png",
+            orange: "/products/5o.png",
+            black: "/products/5bl.png"
+        },
+    },
+    {
+        id: 6,
+        name: "Nike Air Max 270",
+        shortDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, eligendi possimus eum delectus voluptatibus.",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, libero dolorem cumque dicta sunt eaque culpa. Dignissimos ratione inventore officiis impedit expedita explicabo? Neque consectetur libero doloribus quidem magnam suscipit.",
+        price: 59.9,
+        sizes: ["40", "42", "43", "44"],
+        colors: ["gray", "white"],
+        images: {
+            gray: "/products/6g.png",
+            white: "/products/6w.png",
+        },
+    },
+    {
+        id: 7,
+        name: "Nike Ultraboost Pulse",
+        shortDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, eligendi possimus eum delectus voluptatibus.",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, libero dolorem cumque dicta sunt eaque culpa. Dignissimos ratione inventore officiis impedit expedita explicabo? Neque consectetur libero doloribus quidem magnam suscipit.",
+        price: 69.9,
+        sizes: ["40", "42", "43"],
+        colors: ["gray", "pink"],
+        images: {
+            gray: "/products/7g.png",
+            pink: "/products/7p.png",
+        },
+    },
+    {
+        id: 8,
+        name: "Levi's Classic Denim",
+        shortDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, eligendi possimus eum delectus voluptatibus.",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, libero dolorem cumque dicta sunt eaque culpa. Dignissimos ratione inventore officiis impedit expedita explicabo? Neque consectetur libero doloribus quidem magnam suscipit.",
+        price: 59.9,
+        sizes: ["s", "m", "l"],
+        colors: ["blue", "green"],
+        images: {
+            blue: "/products/8b.png",
+            green: "/products/8gr.png"
+        },
+    },
+
 ]
 
 export const ProductList = () => {
