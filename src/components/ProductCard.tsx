03c2f80d8dcf7bc430b89ae9f05820e72f026136
archinit <architbanait1@gmail.com>
2026-03-18 09:49:00 +0530

    Product card updated

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,9 +1,22 @@
 "use client"
 
 import { ProductType } from "@/types"
+import Image from "next/image"
+import Link from "next/link"
 
 export const ProductCard = ({product} : {product: ProductType}) => {
     return <div>
-        ProductCard component
+        <div className="shadow-lg rounded-lg overflow-hidden">
+            <Link href={`/products/${product.id}`}>
+            <div className="relative w-full aspect-[2/3]">
+                <Image 
+                src={product.images[product.colors[0]]} 
+                alt={product.name}
+                fill
+                sizes="(max-width: 768px) 50vw, 25vw" 
+                className="object-cover hover:scale-105 transition-all duration-300"/>
+            </div>
+            </Link>
+        </div>
     </div>
 }
