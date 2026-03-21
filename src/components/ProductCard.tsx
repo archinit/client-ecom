9eac2299075a19d7bb01f02301bc83cc724049b4
archinit <architbanait1@gmail.com>
2026-03-21 18:00:28 +0530

    Product card updated

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,16 +1,29 @@
 "use client"
 
 import { ProductType } from "@/types"
+import { ShoppingCart } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
+import { useState } from "react"
 
 export const ProductCard = ({product} : {product: ProductType}) => {
+    const [productTypes, setProductTypes] = useState({
+        size: product.sizes[0],
+        color: product.colors[0] 
+    })
+
+    const handleProductType = ({type, value}: 
+        {type:"size" | "color", value: string}) => {
+            setProductTypes((prev) => ({
+                ...prev, [type]: value
+            }))
+    }
     return <div>
         <div className="shadow-lg rounded-lg overflow-hidden">
             <Link href={`/products/${product.id}`}>
             <div className="relative w-full aspect-[2/3]">
                 <Image 
-                src={product.images[product.colors[0]]} 
+                src={product.images[productTypes.color]} 
                 alt={product.name}
                 fill
                 sizes="(max-width: 768px) 50vw, 25vw" 
@@ -27,15 +40,42 @@ export const ProductCard = ({product} : {product: ProductType}) => {
                     {/* SIZES */}
                     <div className="flex flex-col gap-1">
                         <span className="text-gray-500">Size</span>
-                        <select name="size" id="size" className="ring ring-gray-300 rounded-md px-2 py-1"> 
+                        <select 
+                            name="size" 
+                            id="size"     
+                            className="ring ring-gray-300 rounded-md px-2 py-1"
+                            onChange={(e) => 
+                                handleProductType({type: "size", value: e.target.value})
+                            }
+                        > 
                             {product.sizes.map(size => (
                                 <option key={size} value={size}>{size.toUpperCase()}</option>
                             ))}
                         </select>
                     </div>
                     {/* COLORS */}
-                    <div className=""></div>
+                    <div className="flex flex-col gap-1">
+                        <span className="text-gray-500">Color</span>
+                        <div className="flex items-center gap-2">
+                            {product.colors.map((color) => (
+                                <div className={`cursor-pointer border ${productTypes.color === color ? "border-gray-400": "border-gray-200"} rounded-full p-px`} key={color} onClick={() => handleProductType({type: "color", value:color})}>
+                                    <div 
+                                        className="w-3.5 h-3.5 rounded-full"
+                                        style={{backgroundColor: color}}
+                                    />  
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
+                {/* PRICE AND ADD TO CART BUTTON */}
+                    <div className="flex items-center justify-between">
+                        <p className="font-medium">${product.price.toFixed(2)}</p>
+                        <button className="ring-1 ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm cursor-pointer hover:text-white hover:bg-black transition-all duration-300 flex items-center gap-2">
+                            <ShoppingCart className="w-4 h-4"/>
+                            Add to Cart
+                        </button>
+                    </div>
             </div>
         </div>
     </div>
